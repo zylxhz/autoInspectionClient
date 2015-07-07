b3f27257141dfe8e52d831432c7f7330d34a53bb
zyl <registerzyl@163.com>
2015-07-07 15:30:50 +0800

    解决了查看最近一次报告和发送最近一次报告的bug

--- src/gui.py
+++ src/gui.py
@@ -37,10 +37,12 @@ class Inspection(wx.Frame):
         mStartInspection = menu1.Append(wx.NewId(), u'开始', u'开始巡检')
         self.menu2 = wx.Menu()
         menuBar.Append(self.menu2, u'报告')   
+        self.mReadLastReport = self.menu2.Append(wx.NewId(), u'查看最近一次报告', u'查看最近一次报告')
+        self.mSendLastReport =  self.menu2.Append(wx.NewId(), u'发送最近一次报告', u'发送最近一次报告')
         self.mReadReport = self.menu2.Append(wx.NewId(), u'查看报告', u'查看报告')
-        self.mSendReport =  self.menu2.Append(wx.NewId(), u'发送报告', u'将报告发送到服务器端')
-        self.menu2.Enable(self.mReadReport.GetId(), False)
-        self.menu2.Enable(self.mSendReport.GetId(), False)
+        self.mSendReport = self.menu2.Append(wx.NewId(), u'发送报告', u'发送报告')
+        self.menu2.Enable(self.mReadLastReport.GetId(), False)
+        self.menu2.Enable(self.mSendLastReport.GetId(), False)
         menu3 = wx.Menu()
         menuBar.Append(menu3, u'工具')
         mSetting = menu3.Append(wx.NewId(), u'配置', u'配置')
@@ -49,6 +51,8 @@ class Inspection(wx.Frame):
         
         self.Bind(wx.EVT_MENU, self.OnStartInspection, mStartInspection)
         self.Bind(wx.EVT_MENU, self.OnSetting, mSetting)
+        self.Bind(wx.EVT_MENU, self.OnSendLastReport, self.mSendLastReport)
+        self.Bind(wx.EVT_MENU, self.OnReadLastReport, self.mReadLastReport)
         self.Bind(wx.EVT_MENU, self.OnSendReport, self.mSendReport)
         self.Bind(wx.EVT_MENU, self.OnReadReport, self.mReadReport)
         self.Bind(wx.EVT_MENU, self.OnTask, mTask)
@@ -90,8 +94,8 @@ class Inspection(wx.Frame):
         arg = '-d ' + out_dir + ' ' + self.script_path
         os.system('pybot ' + arg)
         self.Display(u'报告已生成：' + self.report_path)
-        self.menu2.Enable(self.mReadReport.GetId(), True)
-        self.menu2.Enable(self.mSendReport.GetId(), True)
+        self.menu2.Enable(self.mReadLastReport.GetId(), True)
+        self.menu2.Enable(self.mSendLastReport.GetId(), True)
     
     def OnSetting(self, event):
         dlg = SetDlg(self.remote_server_ip, self.remote_server_port, self.dir_of_report, self.script_path, self.system, self.reporter, self.province, self.city)
@@ -109,27 +113,50 @@ class Inspection(wx.Frame):
         return time.strftime("%Y%m%d%H%M%S",time.localtime(time.time()))
     
     def OnReadReport(self, event):
-#        wildcard = 'html file (*.html)|*.html|All files(*.*)|*.*'
-#        dlg = wx.FileDialog(self, "选择报告", self.dir_of_report, style = wx.OPEN, wildcard = wildcard)
-#        if dlg.ShowModal() == wx.ID_OK:
-#            report_path = dlg.GetPath()
-#            self.Display('报告已选择，路径为：  ' + report_path)
-#            webbrowser.open(report_path) 
-#        dlg.Destroy()
-        webbrowser.open(self.report_path)
+        wildcard = 'html file (*.html)|*.html|All files(*.*)|*.*'
+        dlg = wx.FileDialog(self, "选择报告", self.dir_of_report, style = wx.OPEN, wildcard = wildcard)
+        if dlg.ShowModal() == wx.ID_OK:
+            report_path = dlg.GetPath()
+            self.Display('报告已选择，路径为：  ' + report_path)
+            webbrowser.open(report_path) 
+        dlg.Destroy()
         
+    def OnReadLastReport(self, event):
+        webbrowser.open(self.report_path)        
             
     def OnSendReport(self, event):
-#        wildcard = 'html file (*.html)|*.html|All files(*.*)|*.*'
-#        dlg = wx.FileDialog(self, "发送报告", os.getcwd(), style = wx.OPEN, wildcard = wildcard)
-#        report_path = ''
-#        log_path = ''
-#        if dlg.ShowModal() == wx.ID_OK:
-#            report_path = dlg.GetPath()
-#            self.Display('报告已选择，路径为：  ' + report_path)
-#            log_path = report_path.replace('report.html','log.html')
-#            self.Display('日志文件已选择，路径为：  ' + log_path)                    
-#        dlg.Destroy()
+        wildcard = 'html file (*.html)|*.html|All files(*.*)|*.*'
+        dlg = wx.FileDialog(self, u"发送报告", os.getcwd(), style = wx.OPEN, wildcard = wildcard)
+        report_path = ''
+        log_path = ''
+        if dlg.ShowModal() == wx.ID_OK:
+            report_path = dlg.GetPath()
+            self.Display('报告已选择，路径为：  ' + report_path)
+            log_path = report_path.replace('report.html','log.html')
+            report_file = open(report_path, 'rb')
+            log_file = open(log_path, 'rb')
+            url = r'http://' + self.remote_server_ip + ':' + self.remote_server_port + r'/uploadreport/'
+            # 在 urllib2 上注册 http 流处理句柄
+            register_openers()  
+            # 开始对multipart/form-data编码
+            # headers 包含必须的 Content-Type 和 Content-Length
+            # datagen 是一个生成器对象，返回编码过后的参数
+            datagen, headers = multipart_encode({'system' : self.system, 'province' : self.province, 'city' : self.city, 'reporter' : self.reporter, 'report_file' : report_file, 'log_file' : log_file})
+            # 创建请求对象
+            request = urllib2.Request(url, datagen, headers)
+            # 实际执行请求并取得返回
+            response = urllib2.urlopen(request).read()
+            #巡检报告提交成功或失败的信息
+            info = u'巡检报告提交失败'
+            if response.find(u'巡检报告提交成功'):
+                info = u'巡检报告提交成功'
+            #以消息对话框的方式显示
+            dlgmsg = wx.MessageDialog(None, info, u'消息',wx.OK | wx.ICON_INFORMATION)
+            dlgmsg.Center()
+            dlgmsg.ShowModal()               
+        dlg.Destroy()
+
+    def OnSendLastReport(self, event):
         log_path = self.report_path.replace('report.html', 'log.html')
         report_file = open(self.report_path, 'rb')
         log_file = open(log_path, 'rb')
@@ -154,8 +181,7 @@ class Inspection(wx.Frame):
         dlgmsg = wx.MessageDialog(None, info, u'消息',wx.OK | wx.ICON_INFORMATION)
         dlgmsg.Center()
         dlgmsg.ShowModal()
-        dlgmsg.Destroy()
-              
+        dlgmsg.Destroy()              
     def Display(self, txt):
         str_time = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))
         self.multiText.AppendText(str_time + '    ' + txt + '\n')   
